184efdf7280bce9833c2524704496a109e82dcbd
Amit Patel <redblobgames@gmail.com>
2017-08-04 18:34:55 -0700

    Diagram: dual mesh
    - Added function that can draw the layers of a diagram

--- diagrams.js
+++ diagrams.js
@@ -2,71 +2,41 @@
 // Copyright 2017 Red Blob Games <redblobgames@gmail.com>
 // License: Apache v2.0 <http://www.apache.org/licenses/LICENSE-2.0.html>
 
+'use strict';
+
 const TriangleMesh = require('@redblobgames/triangle-mesh');
 const create_mesh = require('@redblobgames/triangle-mesh/create');
 
-new Vue({
-    el: "#diagram-delaunay",
-    computed: {
-        mesh: function() { return new TriangleMesh(create_mesh(50.0)); }
-    },
-    directives: {
-        draw: function(canvas, binding) {
-            const radius = 4;
-            let {mesh} = binding.value;
-            let ctx = canvas.getContext('2d');
-            ctx.save();
-            ctx.scale(canvas.width/1000, canvas.height/1000);
-            ctx.translate(50, 50);
-            ctx.scale(0.9, 0.9);
-            ctx.fillStyle = "hsl(0, 0%, 80%)";
-            ctx.fillRect(0, 0, 1000, 1000);
-            
+const triangle_center_radius = 4;
+const polygon_center_radius = 5;
+
+function diagram(canvas, mesh, layers) {
+    let ctx = canvas.getContext('2d');
+    ctx.save();
+    ctx.scale(canvas.width/1000, canvas.height/1000);
+    ctx.clearRect(0, 0, 1000, 1000);
+    ctx.translate(50, 50); ctx.scale(0.9, 0.9);
+    ctx.fillStyle = "hsl(0, 0%, 75%)";
+    ctx.fillRect(0, 0, 1000, 1000);
+
+    for (let layer of layers) {
+        switch (layer) {
+        case 'triangle-edges':
             ctx.strokeStyle = "black";
             ctx.lineWidth = 1.0;
             for (let e = 0; e < mesh.num_solid_edges; e++) {
                 let v0 = mesh.e_to_begin_v(e);
                 let v1 = mesh.e_to_end_v(e);
-                ctx.strokeStyle = (mesh.is_ghost_vertex(v0) || mesh.is_ghost_vertex(v1))? "green" : "black";
                 ctx.beginPath();
                 ctx.moveTo(mesh.vertices[v0][0], mesh.vertices[v0][1]);
                 ctx.lineTo(mesh.vertices[v1][0], mesh.vertices[v1][1]);
                 ctx.stroke();
             }
-            
-            ctx.fillStyle = "red";
-            for (let v = 0; v < mesh.num_solid_vertices; v++) {
-                ctx.beginPath();
-                ctx.arc(mesh.vertices[v][0], mesh.vertices[v][1], radius, 0, 2*Math.PI);
-                ctx.fill();
-            }
-            ctx.restore();
-        }
-    }
-});
+            break;
 
-new Vue({
-    el: "#diagram-voronoi",
-    data: {
-        param: 50.0
-    },
-    computed: {
-        mesh: function() { return new TriangleMesh(create_mesh(50.0)); }
-    },
-    directives: {
-        draw: function(canvas, binding) {
-            const radius = 4;
-            let {mesh} = binding.value;
-            let ctx = canvas.getContext('2d');
-            ctx.save();
-            ctx.scale(canvas.width/1000, canvas.height/1000);
-            ctx.translate(50, 50);
-            ctx.scale(0.9, 0.9);
-            ctx.fillStyle = "hsl(0, 0%, 80%)";
-            ctx.fillRect(0, 0, 1000, 1000);
-            
-            ctx.strokeStyle = "black";
-            ctx.lineWidth = 1.5;
+        case 'polygon-edges':
+            ctx.strokeStyle = "white";
+            ctx.lineWidth = 2.0;
             for (let e = 0; e < mesh.num_edges; e++) {
                 let v0 = mesh.e_to_begin_v(e);
                 let v1 = mesh.e_to_end_v(e);
@@ -79,14 +49,90 @@ new Vue({
                     ctx.stroke();
                 }
             }
+            break;
             
-            ctx.fillStyle = "red";
-            for (let v = 0; v < mesh.num_solid_vertices; v++) {
+        case 'triangle-centers':
+            ctx.fillStyle = "blue";
+            ctx.strokeStyle = "hsl(0,0%,75%)";
+            ctx.lineWidth = 3.0;
+            for (let t = 0; t < mesh.num_solid_triangles; t++) {
                 ctx.beginPath();
-                ctx.arc(mesh.vertices[v][0], mesh.vertices[v][1], radius, 0, 2*Math.PI);
+                ctx.arc(mesh.centers[t][0], mesh.centers[t][1], triangle_center_radius, 0, 2*Math.PI);
+                ctx.stroke();
                 ctx.fill();
             }
-            ctx.restore();
+            break;
+            
+        case 'polygon-centers':
+            ctx.fillStyle = "red";
+            ctx.strokeStyle = "hsl(0,0%,75%)";
+            ctx.lineWidth = 3.0;
+            for (let v = 0; v < mesh.num_solid_vertices; v++) {
+                ctx.beginPath();
+                ctx.arc(mesh.vertices[v][0], mesh.vertices[v][1], polygon_center_radius, 0, 2*Math.PI);
+                ctx.stroke();
+                ctx.fill();
+            }
+            break;
+
+        default:
+            console.log('UNKNOWN', layer);
+        }
+    }
+    
+    ctx.restore();
+}
+
+new Vue({
+    el: "#diagram-polygon-centers",
+    computed: {
+        mesh: function() { return new TriangleMesh(create_mesh(75.0)); }
+    },
+    directives: {
+        draw: function(canvas, binding) {
+            diagram(canvas, binding.value.mesh, ['polygon-centers']);
+        }
+    }
+});
+
+new Vue({
+    el: "#diagram-delaunay",
+    computed: {
+        mesh: function() { return new TriangleMesh(create_mesh(75.0)); }
+    },
+    directives: {
+        draw: function(canvas, binding) {
+            diagram(canvas, binding.value.mesh, ['triangle-edges', 'polygon-centers']);
+        }
+    }
+});
+
+new Vue({
+    el: "#diagram-triangle-centers",
+    data: {
+        param: 75.0
+    },
+    computed: {
+        mesh: function() { return new TriangleMesh(create_mesh(75.0)); }
+    },
+    directives: {
+        draw: function(canvas, binding) {
+            diagram(canvas, binding.value.mesh, ['triangle-edges', 'polygon-centers', 'triangle-centers']);
+        }
+    }
+});
+
+new Vue({
+    el: "#diagram-dual-mesh",
+    data: {
+        param: 75.0
+    },
+    computed: {
+        mesh: function() { return new TriangleMesh(create_mesh(75.0)); }
+    },
+    directives: {
+        draw: function(canvas, binding) {
+            diagram(canvas, binding.value.mesh, ['triangle-edges', 'polygon-edges', 'triangle-centers', 'polygon-centers']);
         }
     }
 });
